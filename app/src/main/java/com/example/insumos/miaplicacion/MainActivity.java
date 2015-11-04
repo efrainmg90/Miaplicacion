5f3dcde4f59d510f487c392f7e9c5b35cad4b0eb
efrainmg90 <efrainmg90@gmail.com>
2015-11-04 16:33:51 -0600

    final
    aplicacion terminada

--- app/src/main/java/com/example/insumos/miaplicacion/MainActivity.java
+++ app/src/main/java/com/example/insumos/miaplicacion/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.insumos.miaplicacion;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,18 +17,41 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTimer();
     }
 
     public void onClickStart(View v){
+        running = true;
 
     }
 
     public void onClickStop(View v){
-
+        running = false;
     }
 
     public void onClickReset(View v){
+        running = false;
+        seconds = 0;
+    }
 
+    public void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+
+                String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+                timeView.setText(time);
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 
 
